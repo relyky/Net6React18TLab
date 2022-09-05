8014d25b6b393f82133144ac9b262dff2d34834e
Rely Ky <relyky@hotmail.com>
2022-09-05 18:49:25 +0800

    counter

--- Net6React18TLab/ClientApp/src/views/Demo01/AppForm.tsx
+++ Net6React18TLab/ClientApp/src/views/Demo01/AppForm.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AppForm() {
+  const [count, setCount] = useState(17)
+
   return (
-    <div>
+    <div style={{padding: '0 16px'}}>
       <h1>我是Demo01</h1>
+      <button onClick={_ => setCount(c => c + 1)}>加加</button>
+      <p style={{ fontSize: '2em', color: 'pink' }}>
+        {count}
+      </p>
+
     </div>
   );
 }
