cb57b3bfdd431504285c818b02ab654a43ceb79b
Rely Ky <relyky@hotmail.com>
2022-09-06 11:50:06 +0800

    commit

--- Net6React18TLab/ClientApp/src/views/Demo01/AppForm.tsx
+++ Net6React18TLab/ClientApp/src/views/Demo01/AppForm.tsx
@@ -4,8 +4,9 @@ export default function Demo01_AppForm() {
   const [count, setCount] = useState(17)
 
   return (
-    <div style={{padding: '0 16px'}}>
-      <h1>我是Demo01</h1>
+    <div style={{ padding: '0 16px' }}>
+      <h1>Demo01 : 環境參數與各項機制測試</h1>
+
       <button onClick={_ => setCount(c => c + 1)}>加加</button>
       <p style={{ fontSize: '2em', color: 'pink' }}>
         {count}
--- Net6React18TLab/ClientApp/src/views/Demo02/AppForm.tsx
+++ Net6React18TLab/ClientApp/src/views/Demo02/AppForm.tsx
@@ -6,7 +6,7 @@ export default function Demo02_AppForm() {
 
   return (
     <div style={{ padding: '0 16px' }}>
-      <h1>我是Demo02</h1>
+      <h1>Demo02 : Redux Counter</h1>
 
       <ReduxCounter  />
 
