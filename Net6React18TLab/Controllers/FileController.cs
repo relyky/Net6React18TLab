c41adee4acaf8e7f1f0df660c0feb61c6e37735f
Rely Ky <relyky@hotmail.com>
2023-05-11 11:38:39 +0800

    comment

--- Net6React18TLab/Controllers/FileController.cs
+++ Net6React18TLab/Controllers/FileController.cs
@@ -22,7 +22,7 @@ public class FileController : ControllerBase
   [HttpPost]
   public IActionResult DownloadFile()
   {
-    // [https://blog.miniasp.com/post/2019/12/08/Understanding-File-Providers-in-ASP-NET-Core-and-Download-Physical-File](https://blog.miniasp.com/post/2019/12/08/Understanding-File-Providers-in-ASP-NET-Core-and-Download-Physical-File)
+    // [認識 ASP․NET Core 檔案提供者與透過 Web API 下載實體檔案](https://blog.miniasp.com/post/2019/12/08/Understanding-File-Providers-in-ASP-NET-Core-and-Download-Physical-File)
 
     FileInfo fi = new FileInfo(@"Template/MinIO 評估.docx");
     byte[] blob = System.IO.File.ReadAllBytes(fi.FullName);
